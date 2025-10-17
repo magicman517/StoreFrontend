5bb1698d58a5d1a60584f9843a296296831fd9b4
Illia Vynnyk <illia.vynnyk@pm.me>
2025-10-17 14:39:29 +0300

    shadcn

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 	import '../app.css';
+
 	import favicon from '$lib/assets/favicon.svg';
+
+	import { ModeWatcher } from 'mode-watcher';
+	import { Toaster } from '@/components/ui/sonner';
 
 	let { children } = $props();
 </script>
@@ -8,5 +12,8 @@
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
+
+<ModeWatcher />
+<Toaster />
 
 {@render children?.()}
