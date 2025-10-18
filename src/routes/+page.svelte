1e318790fb0f6112bcb3df7f078b058c10cce0b1
Illia Vynnyk <illia.vynnyk@pm.me>
2025-10-18 13:33:22 +0300

    [feat] input group demo

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import * as InputGroup from '$lib/components/ui/input-group';
+
+	import { SearchIcon } from '@lucide/svelte';
+</script>
+
+<InputGroup.Root class="w-96">
+	<InputGroup.Input placeholder="INPUT GROUP SPECIAL FOR CROCONDINE" />
+	<InputGroup.Addon>
+		<SearchIcon />
+	</InputGroup.Addon>
+	<InputGroup.Addon align="inline-end">
+		<InputGroup.Button>Search</InputGroup.Button>
+	</InputGroup.Addon>
+</InputGroup.Root>
